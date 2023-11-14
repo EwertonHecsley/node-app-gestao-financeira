093cc460f1334e2f0c67a166be11a9a01061155f
EwertonHecsley <ewerton.martinscomercial@gmail.com>
2023-11-14 10:51:35 -0300

    endpoint buscar toddas as transacoes do usuario logado

--- app/Controllers/Http/TransacoesController.ts
+++ app/Controllers/Http/TransacoesController.ts
@@ -2,7 +2,15 @@ import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import Transacoe from 'App/Models/Transacoe'
 
 export default class TransacoesController {
-  public async index({ }: HttpContextContract) { }
+  public async index({ auth, response }: HttpContextContract) {
+    try {
+      const { id } = (await auth.authenticate()).$attributes
+      const transacoes = await Transacoe.query().where({ usuario_id: id })
+      return response.status(200).json(transacoes)
+    } catch (error) {
+      return response.status(500).json({ mensagem: 'Erro interno do servidor', obs: error.message })
+    }
+  }
 
   public async store({ auth, request, response }: HttpContextContract) {
     const { tipo, descricao, valor, categoria_id } = request.body()
